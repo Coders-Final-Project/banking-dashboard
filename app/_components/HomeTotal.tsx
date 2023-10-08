36064e20783b92d9196a744d0916b7c1ff6acb1b
amirmammadov <amirmammado@gmail.com>
2023-10-08 16:59:41 +0400

    first line for home was completed

---
+++ app/_components/HomeTotal.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+
+import "@/sass/components/_homeTotal.scss";
+
+const HomeTotal = () => {
+  return (
+    <div className="home__content__total">
+      <div className="home__content__total__item">
+        <Image
+          src="/assets/home/icon2.png"
+          alt="icon"
+          width={24}
+          height={24}
+          className="home__content__total__item__img"
+        />
+        <div className="home__content__total__item__info">
+          <div className="home__content__total__item__info__text">
+            Total Outstanding
+          </div>
+          <div className="home__content__total__item__info__number">
+            $58,764<span>.25</span>
+          </div>
+        </div>
+      </div>
+      <div className="home__content__total__divider" />
+      <div className="home__content__total__item">
+        <Image
+          src="/assets/home/icon1.png"
+          alt="icon"
+          width={24}
+          height={24}
+          className="home__content__total__item__img"
+        />
+        <div className="home__content__total__item__info">
+          <div className="home__content__total__item__info__text">
+            Upcoming Payment
+          </div>
+          <div className="home__content__total__item__info__number">
+            April 1st, <span>2022</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeTotal;
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,31 @@
 import "@/sass/pages/_home.scss";
 
 import AvatarDetail from "@/shared/AvatarDetail/AvatarDetail";
+import HomeTotal from "./_components/HomeTotal";
+import HomeMethod from "./_components/HomeMethod";
 
 import { user } from "@/db/user";
 
 export default function Home() {
   return (
-    <header className="home__header">
-      <div className="home__header__welcome">
-        <div className="home__header__welcome__title">
-          Good morning, {user.name.split(" ")[0]}
+    <>
+      <header className="home__header">
+        <div className="home__header__welcome">
+          <div className="home__header__welcome__title">
+            Good morning, {user.name.split(" ")[0]}
+          </div>
+          <div className="home__header__welcome__desc">
+            Here is your dashboard overview.
+          </div>
         </div>
-        <div className="home__header__welcome__desc">
-          Here is your dashboard overview.
-        </div>
+        <AvatarDetail {...user} hasBtn />
+      </header>
+      <div className="home__content">
+        <HomeTotal />
+        <HomeMethod />
+        <div className="home__content__payment"></div>
+        <div className="home__content__transaction"></div>
       </div>
-      <AvatarDetail {...user} hasBtn />
-    </header>
+    </>
   );
 }
