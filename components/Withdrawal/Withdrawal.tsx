a132e31dbb00dafeeb2171d9d82b786bb65e8615
amirmammadov <amirmammado@gmail.com>
2023-11-24 23:34:57 +0400

    problem was fixed

---
+++ components/Withdrawal/Withdrawal.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import "@/sass/components/_withdrawal.scss";
+
+const Withdrawal = () => {
+  return <div className="withdrawal">Withdrawal Components</div>;
+};
+
+export default Withdrawal;
