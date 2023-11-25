b42bb7d12cf75fa09ac26417ae3c719f217947f4
Amir Mammadov <110663775+amirmammadov@users.noreply.github.com>
2023-11-25 09:03:19 +0400

    Update page.tsx

--- app/[locale]/(root)/insurance/page.tsx
+++ app/[locale]/(root)/insurance/page.tsx
@@ -2,159 +2,185 @@
 
 import { useEffect, useState } from "react";
 
-import axios from "axios";
-
 import Image from "next/image";
 
-import { FormattedMessage } from "react-intl";
+import "@/sass/pages/_insurance.scss";
+import "@/sass/layout/_pageHeader.scss";
 
-import "@/sass/pages/_signIn.scss";
-import Link from "next/link";
+import axios from "axios";
+import { useGlobalContext } from "@/context/store";
 
 import { useRouter } from "next/navigation";
 
-const initialValues = {
-  email: "",
-  password: "",
-};
+import AvatarDetail from "@/shared/AvatarDetail/AvatarDetail";
+import { insuranceCoverages } from "@/db/insurance";
+import { StateProps } from "@/interface";
+import { useSelector, useDispatch } from "react-redux";
+import { setInsuranceCompleted } from "@/globalRedux/features/appSlice";
 
-const SignIn = () => {
-  const [passwordType, setPasswordType] = useState("password");
-  const [formValues, setFormValues] = useState(initialValues);
-  const [sending, setSending] = useState(false);
-  const [serverError, setServerError] = useState("");
+const Insurance = () => {
+  const [isCoverageOpen, setIsCoverageOpen] = useState(true);
+  const [successAlert, setSuccessAlert] = useState(false);
+
+  const insuranceCompleted = useSelector(
+    (state: StateProps) => state.insuranceCompleted,
+  );
 
   const router = useRouter();
 
+  const dispatch = useDispatch();
+
+  const { data, updateInsuranceCompleted } = useGlobalContext();
+
+  const userCard = useSelector((state: StateProps) => state.userCard);
+
   useEffect(() => {
-    if (serverError !== "") {
+    if (successAlert) {
       setTimeout(() => {
-        setServerError("");
-      }, 1000);
+        setSuccessAlert(false);
+      }, 2000);
     }
-  }, [serverError]);
+  }, [successAlert]);
 
-  const handlePasswordHide = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (passwordType === "password") {
-      setPasswordType("text");
-    } else {
-      setPasswordType("password");
-    }
+  const handleCoverage = () => {
+    setIsCoverageOpen((prevValue) => !prevValue);
   };
 
-  const handleChange = ({
-    target,
-  }: {
-    target: { name: string; value: string };
-  }) => {
-    setFormValues((prevValues) => ({
-      ...prevValues,
-      [target.name]: target.value,
-    }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    const { email, password } = formValues;
-
-    if (!email || !password) {
+  const handleAddInsurance = async () => {
+    if (userCard._id === -1) {
+      router.push("/cards");
       return;
     }
 
     try {
-      setSending(true);
-      const response = await axios.post("/api/user/signin", formValues);
-      console.log(response.data);
-      router.push("/");
-    } catch (error: any) {
-      setServerError(error.message);
-    } finally {
-      setSending(false);
-      setFormValues(initialValues);
+      if (data._id) {
+        const response = await axios.post("/api/insurance", {
+          userID: data._id,
+        });
+
+        dispatch(setInsuranceCompleted(response.data.data));
+        updateInsuranceCompleted(response.data.data);
+        setSuccessAlert(true);
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
   return (
-    <section className="signIn">
-      <form className="signIn__form" onSubmit={handleSubmit}>
-        <div className="signIn__form__title">Log in</div>
-        <div className="signIn__form__item">
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formValues.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="signIn__form__item">
-          <div className="signIn__form__item__passwordLabel">
-            <label htmlFor="pass">Password</label>
-            <button className="toggle__password" onClick={handlePasswordHide}>
-              <Image
-                src={`/assets/sign/${
-                  passwordType === "password" ? "hide" : "see"
-                }.png`}
-                alt="hide"
-                width={passwordType === "password" ? 19 : 24}
-                height={passwordType === "password" ? 15 : 24}
-                className="toggle__password__img"
-              />
-              <p className="toggle__password__text">Hide</p>
-            </button>
-          </div>
-          <input
-            type={passwordType}
-            id="pass"
-            name="password"
-            value={formValues.password}
-            onChange={handleChange}
-          />
-          <div className="remember__me">
-            <input type="checkbox" id="remember" />
-            <label htmlFor="remember">Remember me</label>
+    <main className="insurance">
+      <header className="page__header">
+        <div className="page__header__welcome">
+          <div className="page__header__welcome__title">Get Insured</div>
+          <div className="page__header__welcome__desc">
+            We negotiated exclusive discounts for you, so that you can save
+            money on insurance.
           </div>
         </div>
-        <div className="signIn__form__privacy">
-          By continuing, you agree to the{" "}
-          <Link href="#" className="link">
-            Terms of use
-          </Link>{" "}
-          and{" "}
-          <Link href="#" className="link">
-            Privacy Policy
-          </Link>
-          .
+        <AvatarDetail />
+      </header>
+      <div className="insurance__place">
+        <div className="insurance__place__text">I live in</div>
+        <select className="insurance__place__options">
+          <option value="azerbaijan">Azerbaijan</option>
+          <option value="turkey">Turkey</option>
+        </select>
+      </div>
+      <div className="insurance__content">
+        <div className="insurance__content__card">
+          <div className="insurance__content__card__header">
+            <Image
+              src="/assets/insurance/logo.png"
+              alt="logo"
+              width={110}
+              height={40}
+              className="insurance__content__card__header__img"
+            />
+            <div className="insurance__content__card__header__price">
+              100₼<span>/year</span>
+            </div>
+            <div className="insurance__content__card__header__divider" />
+            {insuranceCompleted ? (
+              <div className="insurance__content__card__header__done">
+                You applied already!
+              </div>
+            ) : (
+              <button
+                className="insurance__content__card__header__btn"
+                onClick={handleAddInsurance}
+              >
+                {userCard._id === -1
+                  ? "Add Card Before Apply"
+                  : "Apply for Coverage"}
+              </button>
+            )}
+          </div>
+          <div className="insurance__content__card__divider" />
+          <div className="insurance__content__card__desc">
+            Genki is an excellent health insurance for digital nomads,
+            freelancers and remote workers. It provides cover from one month up
+            to two years in a monthly subscription that can be cancelled
+            anytime.
+          </div>
+          <div className="insurance__content__card__coverage">
+            <div className="insurance__content__card__coverage__title">
+              <button
+                className="insurance__content__card__coverage__title__btn"
+                onClick={handleCoverage}
+              >
+                What is included in coverage?
+              </button>
+              {isCoverageOpen ? (
+                <Image
+                  src="/assets/insurance/up-arrow.png"
+                  alt="arrow"
+                  width={24}
+                  height={24}
+                  className="insurance__content__card__coverage__title__icon"
+                />
+              ) : (
+                <Image
+                  src="/assets/insurance/down-arrow.png"
+                  alt="arrow"
+                  width={24}
+                  height={24}
+                  className="insurance__content__card__coverage__title__icon"
+                />
+              )}
+            </div>
+            {isCoverageOpen && (
+              <div className="insurance__content__card__coverage__content">
+                {insuranceCoverages.map((coverage) => (
+                  <div
+                    key={coverage.id}
+                    className="insurance__content__card__coverage__content__item"
+                  >
+                    {coverage.value}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+          <div className="insurance__content__card__comment">
+            *Price depends on your age, the region of cover you choose and the
+            deductible you choose
+          </div>
+          <Image
+            src="/assets/insurance/msg.png"
+            alt="message"
+            width={48}
+            height={48}
+            className="insurance__content__card__icon"
+          />
         </div>
-        <button
-          className={`signIn__form__btn ${
-            formValues.email && formValues.password && !sending && "active"
-          }`}
-          disabled={!formValues.email || !formValues.password}
-          type="submit"
-        >
-          {sending ? "Sending..." : "Log In"}
-        </button>
-        <Link href="#" className="signIn__form__forgetPass">
-          Forget your password
-        </Link>
-        <div className="signIn__form__switch">
-          Do not have an acount?{" "}
-          <Link href="/signup" className="singin__switch">
-            Sign Up
-          </Link>
-        </div>
-      </form>
-      {serverError !== "" && (
-        <div className="pop-up pop-up__error">
-          <h2 className="pop-up__text__error">{serverError}</h2>
+      </div>
+      {successAlert && (
+        <div className="insurance__alert--success">
+          Insurance Successfully Applied
         </div>
       )}
-    </section>
+    </main>
   );
 };
 
-export default SignIn;
+export default Insurance;
