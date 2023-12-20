d52185b27c3146c8b44c9f523ec4c6d7ba67c1cb
amirmammadov <amirmammado@gmail.com>
2023-12-20 19:14:04 +0400

    bug related to mock data was resolved

---
+++ db/user.ts
@@ -0,0 +1,59 @@
+export const userPaymentHistory = {
+  "1M": {
+    labels: ["Feb 1", "Feb 8", "Feb 15", "Feb 22", "Feb 28"],
+    datasets: [
+      {
+        label: "1M",
+        data: [40.5, 50.3, 55.5, 60.6, 80.2],
+        backgroundColor: "#0AAF60",
+        borderColor: "#0AAF60",
+      },
+    ],
+  },
+  "3M": {
+    labels: ["Jan", "Feb", "Mar"],
+    datasets: [
+      {
+        label: "3M",
+        data: [330, 320, 335],
+        backgroundColor: "#3981F7",
+        borderColor: "#3981F7",
+      },
+    ],
+  },
+  "6M": {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    datasets: [
+      {
+        label: "6M",
+        data: [300, 280, 290, 285, 310, 315],
+        backgroundColor: "#FFCA29",
+        borderColor: "#FFCA29",
+      },
+    ],
+  },
+  "1Y": {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        label: "1Y",
+        data: [300, 280, 290, 285, 310, 315, 320, 315, 300, 350, 400, 410],
+        backgroundColor: "#F44236",
+        borderColor: "#F44236",
+      },
+    ],
+  },
+};
