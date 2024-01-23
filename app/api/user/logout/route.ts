a4b662e21afafebe7ea4fe590125fff4c8260033
amirmammadov <amirmammado@gmail.com>
2024-01-23 16:37:10 +0400

    http only was added

--- app/api/user/logout/route.ts
+++ app/api/user/logout/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
       success: true,
     });
 
-    response.cookies.set("token", "", { expires: new Date(0) });
+    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
 
     return response;
   } catch (error: any) {
--- middleware.ts
+++ middleware.ts
@@ -102,7 +102,20 @@ export function middleware(request: NextRequest) {
     return response;
   }
 
-  return NextResponse.next();
+  let res = NextResponse.next();
+
+  res.headers.append("Access-Control-Allow-Credentials", "true");
+  res.headers.append("Access-Control-Allow-Origin", "localhost:3000");
+  res.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,DELETE,PATCH,POST,PUT",
+  );
+  res.headers.append(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  );
+
+  return res;
 }
 
 export const config = {
