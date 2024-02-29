5aa2ce4611b7f1954d2dc2833c036792bb1e9f69
amirmammadov <amirmammado@gmail.com>
2024-02-29 12:24:24 +0400

    delete cookie was changed

--- app/api/user/logout/route.ts
+++ app/api/user/logout/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+import { cookies } from "next/headers";
+
 export async function GET() {
   try {
     const response = NextResponse.json({
@@ -10,6 +12,8 @@ export async function GET() {
     // response.cookies.delete("token");
 
     // response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+
+    cookies().delete("token");
 
     response.cookies.set("token", "", {
       httpOnly: true,
