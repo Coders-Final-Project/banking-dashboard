153801d98856a818de806305d9d8db1aa0f3f33a
amirmammadov <amirmammado@gmail.com>
2024-02-29 11:10:53 +0400

    delete cookie was changed

--- app/api/user/logout/route.ts
+++ app/api/user/logout/route.ts
@@ -7,9 +7,9 @@ export async function GET() {
       success: true,
     });
 
-    response.cookies.delete("token");
+    // response.cookies.delete("token");
 
-    // response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
 
     return response;
   } catch (error: any) {
